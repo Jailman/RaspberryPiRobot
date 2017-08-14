df75e93173266542a60a5b1c42161c4188e2dc5b
王志广 <wangzhiguang@pwrd.com>
2017-08-14 10:14:35 +0800

    add servo keybind

--- static/js/keyboardbind.js
+++ static/js/keyboardbind.js
@@ -1,8 +1,21 @@
 
+//driver
+// keycode 65 = a A
+// keycode 68 = d D
+// keycode 83 = s S
+// keycode 87 = w W
+
+//servo
+// keycode 37 = Left
+// keycode 38 = Up
+// keycode 39 = Right
+// keycode 40 = Down
+
 document.onkeydown = function (event) {
     var e = event || window.event;
     var keyCode = e.keyCode || e.which;
     switch (keyCode) {
+        //driver
         case 87:
             $("#robot_up").click();
             break;
@@ -15,6 +28,19 @@ document.onkeydown = function (event) {
         case 68:
             $("#robot_right").click();
             break;
+        //servo
+        case 38:
+            $("#up").click();
+            break;
+        case 40:
+            $("#down").click();
+            break;
+        case 37:
+            $("#left").click();
+            break;
+        case 39:
+            $("#right").click();
+            break;
         default:
             break;
     }
@@ -24,6 +50,7 @@ document.onkeyup = function (event) {
     var e = event || window.event;
     var keyCode = e.keyCode || e.which;
     switch (keyCode) {
+        //driver
         case 87:
             $("#robot_stop").click();
             break;
@@ -36,7 +63,22 @@ document.onkeyup = function (event) {
         case 68:
             $("#robot_stop").click();
             break;
+        //servo
+        case 38:
+            $("#reset").click();
+            break;
+        case 40:
+            $("#reset").click();
+            break;
+        case 37:
+            $("#reset").click();
+            break;
+        case 39:
+            $("#reset").click();
+            break;
         default:
             break;
     }
 }
+
+
