71b0d2744c1dfeed8b1fd762eb61da44e4cb3a79
王志广 <wangzhiguang@pwrd.com>
2017-08-21 13:40:18 +0800

    update iniDB.py

--- Web-Terminal/Modules/initDB.py
+++ Web-Terminal/Modules/initDB.py
@@ -9,7 +9,7 @@ Query data
 """
 
 from sqlalchemy import *
-from sqlalchemy.orm import *
+# from sqlalchemy.orm import *
 
 
 #define egine
@@ -18,28 +18,28 @@ engine = create_engine('sqlite:///../DB/raspberrypi.db')
 metadata = MetaData(engine)
 
 #create tables, init database
-# pilot = Table('pilot', metadata,
-#     Column('id', Integer, primary_key = True),
-#     Column('name', String(255), unique = True),
-#     Column('fullname', String(255)),
-#     Column('email', String(255), unique = True),
-#     Column('password', String(255))
-# )
-#
-# air = Table('air', metadata,
-#     Column('id', Integer, primary_key = True),
-#     Column('date', DateTime, nullable=False),
-#     Column('temperature', Float),
-#     Column('humidity', Float),
-#     Column('infaredetector', Boolean)
-# )
-#
-# joystick = Table('joystick', metadata,
-#     Column('id', Integer, primary_key = True),
-#     Column('gpio', Integer),
-#     Column('function', String(255)),
-#     Column('device', String(255))
-# )
+pilot = Table('pilot', metadata,
+    Column('id', Integer, primary_key = True),
+    Column('name', String(255), unique = True),
+    Column('fullname', String(255)),
+    Column('email', String(255), unique = True),
+    Column('password_hash', String(255))
+)
+
+air = Table('air', metadata,
+    Column('id', Integer, primary_key = True),
+    Column('date', DateTime, nullable=False),
+    Column('temperature', Float),
+    Column('humidity', Float),
+    Column('infaredetector', Boolean)
+)
+
+joystick = Table('joystick', metadata,
+    Column('id', Integer, primary_key = True),
+    Column('gpio', Integer),
+    Column('function', String(255)),
+    Column('device', String(255))
+)
 
 #create tables, if tables exist, pass
 metadata.create_all(engine)
