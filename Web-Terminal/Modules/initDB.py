9a3f69964d405def11454175937204f1b20b9d81
王志广 <wangzhiguang@pwrd.com>
2017-08-21 11:12:26 +0800

    update inidb.py, considering if it's still necessary

--- Web-Terminal/Modules/initDB.py
+++ Web-Terminal/Modules/initDB.py
@@ -13,33 +13,33 @@ from sqlalchemy.orm import *
 
 
 #define egine
-engine = create_engine('sqlite:///../DB/raspberrypirobot.db')
+engine = create_engine('sqlite:///../DB/raspberrypi.db')
 #bind metadata
 metadata = MetaData(engine)
 
 #create tables, init database
-pilot = Table('pilot', metadata,
-    Column('id', Integer, primary_key = True),
-    Column('name', String(255), unique = True),
-    Column('fullname', String(255)),
-    Column('email', String(255), unique = True),
-    Column('password', String(255))
-)
-
-air = Table('air', metadata, 
-    Column('id', Integer, primary_key = True),
-    Column('date', DateTime, nullable=False),
-    Column('temperature', Float),
-    Column('humidity', Float),
-    Column('infaredetector', Boolean)
-)
-
-joystick = Table('joystick', metadata,
-    Column('id', Integer, primary_key = True),
-    Column('gpio', Integer),
-    Column('function', String(255)),
-    Column('device', String(255))
-)
+# pilot = Table('pilot', metadata,
+#     Column('id', Integer, primary_key = True),
+#     Column('name', String(255), unique = True),
+#     Column('fullname', String(255)),
+#     Column('email', String(255), unique = True),
+#     Column('password', String(255))
+# )
+#
+# air = Table('air', metadata,
+#     Column('id', Integer, primary_key = True),
+#     Column('date', DateTime, nullable=False),
+#     Column('temperature', Float),
+#     Column('humidity', Float),
+#     Column('infaredetector', Boolean)
+# )
+#
+# joystick = Table('joystick', metadata,
+#     Column('id', Integer, primary_key = True),
+#     Column('gpio', Integer),
+#     Column('function', String(255)),
+#     Column('device', String(255))
+# )
 
 #create tables, if tables exist, pass
 metadata.create_all(engine)
@@ -49,7 +49,7 @@ conn = engine.connect()
 
 def insert_data(table, data):
     i = table.insert()
-    r1 = conn.execute(i, data)
+    conn.execute(i, data)
     # print r1.inserted_primary_key
 #func usage
 # data = dict(name='jailman', fullname='jailman lobo', email='jailman@sina.com', password='damnyou')
